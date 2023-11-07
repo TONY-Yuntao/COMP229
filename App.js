c396844e0151bb75357ad91c17ffa3eb960d29b0
Tony Zhang <yzhanwork@hotmail.com>
2023-11-06 20:56:40 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+function MyForm() {
+
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Form submitted:', { name, description });
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <form onSubmit={handleSubmit} style={{ width: '300px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
+      <div>
+        <p style={{ marginLeft:'100px'}}>New Shop</p>
+      </div>
+      <br></br>
+
+        <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <br></br> <br></br>
+      <div>
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+      </div>
+      <br></br>
+      <div>
+      <button type="submit" style={{ backgroundColor: 'blue', color: 'white', marginRight: '100px' }}>
+            Submit
+          </button>
+          <button type="button" style={{ backgroundColor: 'gray', color: 'black' }}>
+          Cancel
+        </button>
+      </div>
+    </form>
+    </div>
+  );
+}
+
+export default MyForm;
